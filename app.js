0ac0601c26fec0d23f939d21be056b05fe057cb8
Will <apricotsenator@gmail.com>
2018-03-10 10:13:06 -0500

    more-better

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-var gifs = ["dog", "cat", "hamster", "parrot"]
+var gifs = ["guitar", "saxaphone", "violin", "piano","cello","flute","tuba","cymbal","synthesizer"]
 
 function searchGiphy() {
 
@@ -10,37 +9,21 @@ function searchGiphy() {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-       var results = response.data;
+        var results = response.data;
         console.log(results);
         $("#gif-display").text("");
-        // Looping over every result item
+
         for (var i = 0; i < results.length; i++) {
-            // Creating a div with the class "item"
             var gifDiv = $("<div class='item'>");
-
-            // Storing the result item's rating
             var rating = results[i].rating;
-
-            // Creating a paragraph tag with the result item's rating
             var p = $("<p>").text("Rating: " + rating);
-
-            // Creating an image tag
             var personImage = $("<img>");
-
-            // Giving the image tag an src attribute of a proprty pulled off the
-            // result item
             personImage.attr("src", results[i].images.fixed_height.url);
-
-            // Appending the paragraph and personImage we created to the "gifDiv" div we created
             gifDiv.append(p);
             gifDiv.append(personImage);
-
-            // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
             $("#gif-display").prepend(gifDiv);
-
         }
         makebuttons();
-        
     });
 }
 
@@ -71,5 +54,3 @@ $("#add-gif").on("click", function (event) {
 
 $(document).on("click", ".gif", searchGiphy);
 makebuttons();
-
-
